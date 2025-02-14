05e34b975c57b211bd71618e6431bbc02481e585
DevBet <frana00@gmail.com>
2025-02-13 22:08:52 -0300

    Patrón adapter: Composición (objetos)

---
+++ Adapter_Composicion/src/objeto_pila_lista/Cliente.java
@@ -0,0 +1,17 @@
+package objeto_pila_lista;
+
+public class Cliente {
+
+
+    public Cliente() {
+
+    }
+
+    public static void main(String[] args) {
+
+
+        Pila pila2 = new AdaptadorListaAPila();
+        pila2.push(1);
+        pila2.pop();
+    }
+}
